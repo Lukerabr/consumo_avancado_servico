27c287081e1bd62faf03c72bbada38213ab49281
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-05 10:57:01 -0400

    Recuperando os  dados de uma API e colocar em uma lista.
    NO FINAL COLOCAR "return Container();"

--- lib/Home.dart
+++ lib/Home.dart
@@ -3,6 +3,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
+import 'Post.dart';
+
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -13,45 +15,68 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
-  Future<Map> _recuperarPreco() async {
-    String url = "https://www.blockchain.com/ticker";
-    http.Response response = await http.get(Uri.parse(url));
-    return json.decode(response.body);
+  String _urlBase = "https://jsonplaceholder.typicode.com";
+
+
+  Future<List<Post>> _recuperarPostagens() async{
+
+    http.Response response = await http.get(Uri.parse(_urlBase + "/posts"));
+    var dadosJson = json.decode(response.body);
+
+    List<Post> postagens = [];
+
+    for(var post in dadosJson){
+      print("post: " + post["title"]);
+      Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
+      postagens.add(p);
+    }
+
+    return postagens;
+    //print(postagens.toString());
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-      future: _recuperarPreco(),
-      builder: (contex, sanpshot){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de serviços avançado"),
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: _recuperarPostagens(),
+        builder: (contex, snapshot){
 
-        String resultado = "";
+          switch(snapshot.connectionState){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            case ConnectionState.active:
+            case ConnectionState.done:
+              if(snapshot.hasError){
+                print("Lista: Erro ao carregar!");
+              }else{
+                print("Lista: Carregou!");
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index){
 
-        switch(sanpshot.connectionState){
-          case ConnectionState.none:
-            print("Conexão NONE");
-            break;
-          case ConnectionState.waiting:
-            print("Conexão WAITING");
-            resultado = "Carregando...";
-            break;
-          case ConnectionState.active:
-            print("Conexão ACTIVE");
-            break;
-          case ConnectionState.done:
-            print("Conexão DONE");
-            if(sanpshot.hasError){
-              resultado = "Erro ao carregar os dados.";
-            }else{
-              double valor = sanpshot.data!["BRL"]["buy"];
-              resultado = "Preço do Bitcoin: ${valor.toString()}";
-            }
-            break;
-        }
-        return Center(
-          child: Text(resultado),
-        );
-      },
+                      List<Post>? lista = snapshot.data;
+                      Post post  = lista![index];
+
+                      return ListTile(
+                        title: Text(post.title),
+                        subtitle: Text(post.id.toString()),
+                      );
+                    }
+                );
+              }
+              break;
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
