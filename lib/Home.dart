40d1145718f1b7788230bf8e8136090bd81b3b65
Lucca D'Amico Diniz <lucca.damico@hotmail.com>
2021-09-05 11:24:24 -0400

    Criando método post.

--- lib/Home.dart
+++ lib/Home.dart
@@ -26,7 +26,7 @@ class _HomeState extends State<Home> {
     List<Post> postagens = [];
 
     for(var post in dadosJson){
-      print("post: " + post["title"]);
+      //print("post: " + post["title"]);
       Post p = Post(post["userId"], post["id"], post["title"], post["body"]);
       postagens.add(p);
     }
@@ -36,46 +36,106 @@ class _HomeState extends State<Home> {
 
   }
 
+  _post() async {
+
+    var corpo = json.encode(
+        {
+          "userId": 120,
+          "id": null,
+          "title": "Titulo",
+          "body": "Corpo da postagem"
+        }
+    );
+    http.Response response = await http.post(
+      Uri.parse(_urlBase + "/posts"),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+      body: corpo
+    );
+
+  print("Resposta: ${response.statusCode}");
+  print("resposta: ${response.body}");
+
+  }
+
+  _put(){
+
+  }
+
+  _patch(){
+
+  }
+
+  _delete(){
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Consumo de serviços avançado"),
       ),
-      body: FutureBuilder<List<Post>>(
-        future: _recuperarPostagens(),
-        builder: (contex, snapshot){
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                ElevatedButton(
+                    onPressed: _post,
+                    child: Text("Salvar")
+                ),
+                ElevatedButton(
+                    onPressed: _post,
+                    child: Text("Atualizar")
+                ),
+                ElevatedButton(
+                    onPressed: _post,
+                    child: Text("Remover")
+                )
+              ],
+            ),
+            Expanded(
+                child: FutureBuilder<List<Post>>(
+                  future: _recuperarPostagens(),
+                  builder: (context, snapshot){
 
-          switch(snapshot.connectionState){
-            case ConnectionState.none:
-            case ConnectionState.waiting:
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            case ConnectionState.active:
-            case ConnectionState.done:
-              if(snapshot.hasError){
-                print("Lista: Erro ao carregar!");
-              }else{
-                print("Lista: Carregou!");
-                return ListView.builder(
-                  itemCount: snapshot.data!.length,
-                    itemBuilder: (context, index){
+                    switch(snapshot.connectionState){
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      case ConnectionState.active:
+                      case ConnectionState.done:
+                        if(snapshot.hasError){
+                          print("Lista: Erro ao carregar!");
+                        }else{
+                          return ListView.builder(
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (context, index){
 
-                      List<Post>? lista = snapshot.data;
-                      Post post  = lista![index];
+                                List<Post>? lista = snapshot.data;
+                                Post post  = lista![index];
 
-                      return ListTile(
-                        title: Text(post.title),
-                        subtitle: Text(post.id.toString()),
-                      );
+                                return ListTile(
+                                  title: Text(post.title),
+                                  subtitle: Text(post.id.toString()),
+                                );
+                              }
+                          );
+                        }
+                        break;
                     }
-                );
-              }
-              break;
-          }
-          return Container();
-        },
+                    return Container();
+                  },
+                ),
+            )
+          ],
+        ),
       ),
     );
   }
